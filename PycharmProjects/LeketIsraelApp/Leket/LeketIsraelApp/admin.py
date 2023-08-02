92788c8e511cad83b4ccfb4748e5d9f05d4ca8be
ShirleyEshto <103575598+ShirleyEshto@users.noreply.github.com>
2023-08-02 11:21:04 +0300

    remove duplicated

--- PycharmProjects/LeketIsraelApp/Leket/LeketIsraelApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import leket_DB
-
-admin.site.register(leket_DB)
--- PycharmProjects/LeketIsraelApp/Leket/LeketIsraelApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeketisraelappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LeketIsraelApp'
